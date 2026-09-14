cfe9e9dc84c20b9db258d213529343b8b91cb9a0
viv <putiacha@gmail.com>
2026-09-14 23:33:13 +0700

    feat(booking): added footer

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -31,9 +31,11 @@
             <main>
                 @yield('content', $slot ?? '')
             </main>
+
+            <!-- Page Footer -->
+            <x-footer />
         </div>
     </body>
-
 
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
